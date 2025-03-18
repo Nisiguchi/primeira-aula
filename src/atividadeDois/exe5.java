f5edeb0c04358dfb8aab3a3cdb853f6e4aad9ae8
Nicole <nlinonicole@gmail.com>
2025-03-18 20:33:02 -0300

    exercicio 5

---
+++ src/atividadeDois/exe5.java
@@ -0,0 +1,21 @@
+package atividadeDois;
+
+import java.util.Scanner;
+
+public class exe5 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int numero, triplo;
+        int contar = 0;
+
+        while(contar <= 9) {
+        System.out.println("Digite 10 números e irei te mostrar o triplo dele: ");
+        numero = entrada.nextInt();
+        triplo = numero * 3;
+
+        System.out.println("O triplo é: " + triplo);
+        contar++;
+        }
+    }
+}
